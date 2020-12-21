0bc0915ffadce91133fe5df62b0911fac0856233
Dominvsky <hejdominowy@outlook.com>
2020-12-21 20:16:12 +0100

    Version.1

--- N.2/N.2/Program.cs
+++ N.2/N.2/Program.cs
@@ -6,7 +6,31 @@ namespace N._2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] tab = new int[10];
+            string a;
+
+            for (int i = 0; i < 10; i++)
+            {
+                a = Console.ReadLine();
+                bool CheckA = int.TryParse(a, out _);
+                
+                if (CheckA == true) 
+                { 
+                    tab[i] = int.Parse(a);
+                }
+                else 
+                {
+                    Console.WriteLine("To nie jest liczba");
+                    Console.WriteLine("Podaj Jeszcze raz ale liczbe:");
+                    a = Console.ReadLine();
+                    tab[i] = int.Parse(a);
+                }
+            }
+
+            for (int j = 0; j < 10; j++)
+            {
+                Console.WriteLine("tab[" + j + "] = " + tab[j]);
+            }
         }
     }
 }
---
+++ N.3/N.3/Program2.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace N._3
+{
+    public class Program2
+    {
+        public int x;
+        public int[] y = new int[2];
+
+        public void wypisz()
+        {
+            
+        }
+    }
+}
