8294b38dcae9b60f087f78d40662f7088d5638d4
Dominvsky <hejdominowy@outlook.com>
2021-01-28 23:40:51 +0100

    Init

---
+++ N4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace N4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczba;
+            liczba = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(liczba);
+        }
+    }
+}
